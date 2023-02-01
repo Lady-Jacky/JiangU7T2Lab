073c108600f60b063142fe9b37ff503590d251da
1E10 Laptop <sample@bths.edu>
2023-02-01 10:24:00 -0500

    Finished

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Runner {
+import java.util.ArrayList;
+
+public class Runner {
+    public static void main(String[] args) {
+        ArrayList<String> letters = new ArrayList<String>();
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+
+        numList.add(3);
+
+        numList.add(0, 6);
+
+        numList.add(7);
+
+        numList.add(2, 8);
+
+        numList.set(3, 9);
+
+        numList.add(1, 2);
+
+        numList.add(1, numList.size());
+
+        numList.remove(4);
+
+        numList.set(4, 0);
+
+        System.out.print(numList);
+
+    }
 }
